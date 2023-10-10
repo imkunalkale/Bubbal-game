14ea43d6eeb4e305f6c5aedf472e7177a8569e46
kunal kale <kunalkale7080@gmail.com>
2023-10-10 15:26:27 +0530

    first commit

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
-var clutter = "";
+var timer = 60;
 
-for (i = 1; i <= 85; i++) {
-  clutter += ` <div class="bubbal">5</div>`;
+function getNewHit(){
+ var rn = Math.floor(Math.random()*10);
+ document.querySelector("#hitval").textContent = rn;
 }
 
-document.querySelector("#pbtm").innerHTML = clutter;
+function Makebubble() {
+  var clutter = "";
+  for (i = 1; i <= 85; i++) {
+    var rn = Math.floor(Math.random() * 10);
+    clutter += `<div class="bubble">${rn}</div>`;
+  }
+
+  document.querySelector("#pbtm").innerHTML = clutter;
+}
+
+function runTimer() {
+  var timerint = setInterval(function () {
+    if (timer > 0) {
+      timer--;
+      document.querySelector("#timerval").textContent = timer;
+    } else {
+      clearInterval(timerint);
+    }
+  }, 1000);
+}
+
+getNewHit();
+Makebubble();
+runTimer();
